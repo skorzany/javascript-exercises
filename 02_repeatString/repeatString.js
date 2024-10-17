7b92f50f7159f14317fa85e40b5dc90de831a6c6
skorzany <177423232+skorzany@users.noreply.github.com>
2024-10-17 15:14:39 +0200

    Solve exercise 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(txt, num) {
+    if (num < 0) return "ERROR";
+    
+    let answer = [];
+    for (let i = 0; i < num; i++) {
+        answer.push(txt);
+    }
+    return answer.join('');
 };
 
 // Do not edit below this line
