8ee8ade09cdaa6871ae2da664e5c3a8f3db374ff
skorzany <177423232+skorzany@users.noreply.github.com>
2024-10-17 19:01:47 +0200

    Solve exercise 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(F) {
+  return Number(((F - 32)/1.8).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(C) {
+  return Number((1.8*C + 32).toFixed(1));
 };
 
 // Do not edit below this line
