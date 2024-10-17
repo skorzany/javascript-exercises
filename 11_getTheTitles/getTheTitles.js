584ccd75da697dcc206283cec02bfe9678ef3e8b
skorzany <177423232+skorzany@users.noreply.github.com>
2024-10-17 19:58:34 +0200

    Solve exercise 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    let titles = [];
+    for (obj of arr) titles.push(obj["title"]);
+    return titles;
 };
 
 // Do not edit below this line
