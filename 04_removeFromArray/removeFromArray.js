710b51da9a70683829659667219650e8a02403e6
skorzany <177423232+skorzany@users.noreply.github.com>
2024-10-17 15:44:18 +0200

    Solve exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(...params) {
+    const arr = params[0];
+    const unwanted = params.slice(1);
+    let answer = [];
+    for (element of arr) {
+        if (!unwanted.includes(element)) answer.push(element);
+    }
+    return answer;
 };
 
 // Do not edit below this line
