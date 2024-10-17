3b0eeaec49c1e9c13af36ee37e4040d1b3b96268
skorzany <177423232+skorzany@users.noreply.github.com>
2024-10-17 16:29:47 +0200

    Solve exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (start < 0 || end < 0) return "ERROR";
+    if (start%1 !== 0 || end%1 !== 0) return "ERROR";
+    if (typeof start !== "number" || typeof end !== "number") return "ERROR";
 
+    let total = 0;
+    if (end < start) [start, end] = [end, start];
+    for (let i = start; i <= end; i++) {
+        total += i;
+    }
+    return total;
 };
 
 // Do not edit below this line
