a359335f351614167fe99476e7d55f431c238e31
skorzany <177423232+skorzany@users.noreply.github.com>
2024-10-17 20:30:50 +0200

    Solve exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    let oldestPerson = {};
+    let oldestAge = -1;
 
+    for (obj of arr) {
+        const age = (obj["yearOfDeath"] || new Date().getFullYear()) - obj["yearOfBirth"]
+        if (oldestAge < age) {
+            oldestAge = age;
+            oldestPerson = obj;
+        }
+    }
+    return oldestPerson;
 };
 
 // Do not edit below this line
