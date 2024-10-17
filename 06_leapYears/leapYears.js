ea54f66aea34c50b7d0d5e5c653c4b60a478359a
skorzany <177423232+skorzany@users.noreply.github.com>
2024-10-17 18:50:08 +0200

    Solve exercise 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,7 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year%4) return false;
+    if (year%100) return true;
+    return year%400 === 0;
 };
 
 // Do not edit below this line
